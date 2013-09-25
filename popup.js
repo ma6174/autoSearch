03165c0f0a23a1ea303b36048a5c1905dc192e28
ma6174 <ma6174@163.com>
2013-09-25 17:11:31 +0800

    基本功能实现

---
+++ popup.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+    ele = document.getElementById("ma6174-keyword");
+    ele.value = localStorage['ma6174-keyword'].replace(/\|/g,"\n");
+    ele.onkeyup= function(){
+        var val=this.value.replace(/\s+/g,"|");
+        localStorage['ma6174-keyword']=val;
+        chrome.storage.local.set({keyword:val},function (){
+            console.log("Storage Succesful");
+        });
+    };
+};
