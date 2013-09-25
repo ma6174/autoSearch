0523e8558ee206f73ba2492be08ebdd8eea2d22c
ma6174 <ma6174@163.com>
2013-09-25 19:37:32 +0800

    将高亮模块换成jquery的

--- background.js
+++ background.js
@@ -1,7 +1,9 @@
-window.onload = function(){
+function highlight(){
     chrome.storage.local.get(null,function (obj){
-        var reg = new RegExp("("+obj.keyword+")(?=[^<>]*<)","ig");
-        document.body.innerHTML = document.body.innerHTML.replace(reg,"<span class='ma6174' style='color:red;background-color:#ff0;'>$1</span>");
+        var keys = obj.keyword.split("|");
+        for(var i in keys){
+            $("body").highlight(keys[i]);
+        }
     });
 };
-
+window.onload = highlight();
