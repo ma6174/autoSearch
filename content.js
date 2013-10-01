5ac6495056086535a9a7b60518c38f34f1aed5b8
ma6174 <ma6174@163.com>
2013-10-01 11:43:11 +0800

    增加打开链接和搜索功能

--- content.js
+++ content.js
@@ -6,8 +6,37 @@ function highlight(){
         }
     });
 };
-function highlightSelection(){
+function openURL(txt){
+    var re=/^((http|https|ftp):\/\/)?(\w(\:\w)?@)?([0-9a-z_-]+\.)*?([a-z0-9-]+\.[a-z]{2,6}(\.[a-z]{2})?(\:[0-9]{2,6})?)((\/[^?#<>\/\\*":]*)+(\?[^#]*)?(#.*)?)?$/i;
+    $("#ma6174-tip").empty();
+    $("#ma6174-tip").show();
+    $("#ma6174-tip").css({
+        "top":event.pageY,
+        "left":event.pageX
+    });
+    if(re.test(txt)){
+        if(txt.search("://") == -1){
+            txt = "http://" + txt;
+        }
+        $("<a></a>").text("打开连接").attr({
+            "href":txt,
+            "target":"_blank"
+        }).appendTo($("#ma6174-tip"));
+    }else{
+        $("<a></a>").text("谷歌搜索").attr({
+            "href":"https://www.google.com/#q="+txt,
+            "target":"_blank"
+        }).appendTo($("#ma6174-tip"));
+        $("<br />").appendTo($("#ma6174-tip"));
+        $("<a></a>").text("百度搜索").attr({
+            "href":"http://baidu.com/s?wd="+txt,
+            "target":"_blank"
+        }).appendTo($("#ma6174-tip"));
+    }
+}
+function highlightSelection(event){
     var selectedText;
+    $("#ma6174-tip").hide();
     if(window.getSelection){
         selectedText=window.getSelection();
     }else if(document.selection){
@@ -15,15 +44,17 @@ function highlightSelection(){
     }
     if(selectedText!=""){
         chrome.extension.sendRequest({ text:selectedText.toString()});
+        openURL(selectedText.toString());
         $("body").highlight(selectedText.toString(),{ element: 'span', className: 'ma6174-highlight' });
     }
 }
 $().ready(function(){
     highlight();
+    $("<div id='ma6174-tip'></div>").appendTo($("body"));
 });
-$("body").click(function(){
+$("body").click(function(event){
     $(this).unhighlight({ element: 'span', className: 'ma6174-highlight' });
     highlight();
-    highlightSelection();
+    highlightSelection(event);
     return true;
 });
